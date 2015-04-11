3ae7dd9e81858d811f3caf8b3c0a02731749bcad
Xenofon Spafaridis <nohponex@gmail.com>
2015-04-11 23:59:51 +0300

    Correct use of MetaphraseException

--- demo.php
+++ demo.php
@@ -5,13 +5,17 @@ require 'vendor/autoload.php';
 $settings = [
     'API_KEY' => '614a98a31bd17b3a82094ef0388b9d81',
     'language_code' => 'en',
-    'project_id' => 19
+    'project_id' => 2222
 ];
 
-
+try{
 $metaphrase = new metaphrase\phpsdk\Metaphrase($settings['API_KEY']);
 
 $translated = $metaphrase->fetch(
     $settings['project_id'], $settings['language_code']);
 
+}catch(metaphrase\phpsdk\MetaphraseException $e){
+    print( $e->getMessage( ) );
+    die();
+}
 print_r($translated);

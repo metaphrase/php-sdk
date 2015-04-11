743148136db8bd3fc578da9578fd55994db4da84
Xenofon Spafaridis <nohponex@gmail.com>
2015-04-12 01:49:17 +0300

    Update readme

--- demo.php
+++ demo.php
@@ -1,21 +1,24 @@
 <?php
+/**
+ * Demo
+ */
 
+//Or replace your path to vendor
 require 'vendor/autoload.php';
 
 $settings = [
     'API_KEY' => '614a98a31bd17b3a82094ef0388b9d81',
     'language_code' => 'en',
-    'project_id' => 2222
+    'project_id' => 19
 ];
 
-try{
-$metaphrase = new metaphrase\phpsdk\Metaphrase($settings['API_KEY']);
+try {
+    $metaphrase = new metaphrase\phpsdk\Metaphrase($settings['API_KEY']);
 
-$translated = $metaphrase->fetch(
-    $settings['project_id'], $settings['language_code']);
-
-}catch(metaphrase\phpsdk\MetaphraseException $e){
-    print( $e->getMessage( ) );
+    $translated = $metaphrase->fetch(
+        $settings['project_id'], $settings['language_code']);
+} catch (metaphrase\phpsdk\MetaphraseException $e) {
+    print( $e->getMessage());
     die();
 }
 print_r($translated);
